55c7cc047db0a43c669c52d916e1cfc96b734f92
AdwaitAdk <adwaitadk@tuta.io>
2022-03-04 11:55:35 +0545

    Added comments

--- bsToAd.py
+++ bsToAd.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Websites which actually converts the date
 url = "https://www.ashesh.com.np/nepali-date-converter.php"
+
+# Function to change the month variable to website readable string
 def monthSelect(arg):
     months = {
         1 : "Baishakh",
@@ -17,6 +20,7 @@ def monthSelect(arg):
         11 : "Falgun",
         12 : "Chaitra"
     }
+    # Returns the argument specific month; If it fails, returns Baishakh
     return months.get(arg, "Baishakh")
 
 year = input("Year: ")
@@ -25,13 +29,19 @@ day = input("Day: ")
 
 month = str(monthSelect(int(month)))
 
+# Post request attributes for the website
 attributes = {
     "year": year,
     "month" : month,
     "day" : day,
     "submit" : "Convert"        
 }
+
+# Making a post request with the specified attributes and getting the data
 response = requests.post(url, data=attributes)
+
+# Parsing the data using the BeautifulSoup library
 soup = BeautifulSoup(response.content, "lxml")
 
+# Formatting the data to display only the date
 print(soup.find_all("div", class_="inner")[1].text[3:])
