717a4a829ccde39fd630aa613698273c17c5bf98
AdwaitAdk <adwaitadk@tuta.io>
2022-03-10 13:11:48 +0545

    Added comments

--- pushToTalk.py
+++ pushToTalk.py
@@ -1,20 +1,31 @@
-from pynput import keyboard
-import os
+from pynput import keyboard # For keyboard listen event
+import os # For running system commands
 
+# Keypress event
 def onPress(key):
+    # Checks if key is key.alt_r
     if key == keyboard.Key.alt_r:
+        # Unmutes the microphone
         os.system('pactl set-source-mute @DEFAULT_SOURCE@ false')
 
+# Key release event
 def onRelease(key):
+    # Checks if key is key.alt_r
     if key == keyboard.Key.alt_r:
+        # Mutes the microphone
         os.system('pactl set-source-mute @DEFAULT_SOURCE@ true')
 
 try:
+    # Set the microphone to mute by default
     print('Setting source to mute by default...')
     os.system('pactl set-source-mute @DEFAULT_SOURCE@ true')
+
+    # Listen for key events
     with keyboard.Listener(on_press=onPress, on_release=onRelease) as listener:
         listener.join()
+
 except:
+    # Sets the microphone to unmute
     print('Reverting back...')
     os.system('pactl set-source-mute @DEFAULT_SOURCE@ false')
     print('Goodbye!')
