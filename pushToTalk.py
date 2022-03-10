80005fa8fc9b545c76caefbd672b1a6b7c300fcc
AdwaitAdk <adwaitadk@tuta.io>
2022-03-10 15:04:25 +0545

    Added system notification support

--- pushToTalk.py
+++ pushToTalk.py
@@ -1,5 +1,6 @@
 from pynput import keyboard # For keyboard listen event
 import os # For running system commands
+import sys
 
 # Keypress event
 def onPress(key):
@@ -18,14 +19,15 @@ def onRelease(key):
 try:
     # Set the microphone to mute by default
     print('Setting source to mute by default...')
+    os.system('notify-send "Microphone" "Setting source to mute by default..."')
     os.system('pactl set-source-mute @DEFAULT_SOURCE@ true')
-
     # Listen for key events
     with keyboard.Listener(on_press=onPress, on_release=onRelease) as listener:
         listener.join()
-
 except:
     # Sets the microphone to unmute
     print('Reverting back...')
+    os.system('notify-send "Microphone" "Reverting back..."')
     os.system('pactl set-source-mute @DEFAULT_SOURCE@ false')
     print('Goodbye!')
+    sys.exit()
